6fa798a99ad456a664c40cabb3cc4436066de5f5
juanpablopolo13@gmail.com <juanpablopolo13@gmail.com>
2025-05-18 02:40:48 +0200

    cambio estilo panel admin y logica crear usuario funcional

--- src/pages/admin-panel/admin-panel.component.ts
+++ src/pages/admin-panel/admin-panel.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../../components/logo/logo.component';
 import { AuthService } from '../../services/auth.service'; // Importando la clase authservice
 import { inject } from '@angular/core';
-import { GroupComponent } from '../group/group.component'; 
+import { GroupComponent } from '../group/group.component';
 import { TurnComponent } from '../turn/turn.component';
+import { CreateNewUserComponent } from '../create-new-user/create-new-user.component';
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
-  imports: [CommonModule, LogoComponent, GroupComponent, TurnComponent],
+  imports: [CommonModule, LogoComponent, GroupComponent, TurnComponent, CreateNewUserComponent],
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.css']
 })
@@ -17,8 +18,9 @@ export class AdminPanelComponent {
 
   @ViewChild(GroupComponent) groupComp!: GroupComponent; // Instancia de componenete grup parapoder usar el metodo de renderizacion de popup
   @ViewChild(TurnComponent) turnComp!: TurnComponent; // Instancia de componenete turn parapoder usar el metodo de renderizacion de popup
+  @ViewChild(CreateNewUserComponent) userComp!: CreateNewUserComponent; // Instancia de componenete user parapoder usar el metodo de renderizacion de popup
 
-    private authService = inject(AuthService); // Creando una instancia de Authservice para poder usar su metodos
+  private authService = inject(AuthService); // Creando una instancia de Authservice para poder usar su metodos
 
   activeMenu: string | null = null;
   userName: string = ''; // Variable para guardar el nombre que se renderiza
@@ -27,11 +29,16 @@ export class AdminPanelComponent {
   openGroupDialog() {
     this.groupComp.showDialog();
   }
+  openUserDialog() {
+    this.userComp.showDialog();
+  }
 
   // metodo para llamar al metodo del componente turn que renderiza el popup
   openTurnDialog() {
     this.turnComp.showDialog();
   }
+
+
 
   toggleMenu(menu: string) {
     this.activeMenu = this.activeMenu === menu ? null : menu;
@@ -39,11 +46,11 @@ export class AdminPanelComponent {
 
   ngOnInit() { // metodo para recuperar el nombre
     const token = this.authService.getToken();
-  if (token) {
-    this.userName = token; // Si el token existe, se asigna a userName
-  } else {
-    this.userName = ''; // Si el token es null, asignar un valor por defecto
-  }
+    if (token) {
+      this.userName = token; // Si el token existe, se asigna a userName
+    } else {
+      this.userName = ''; // Si el token es null, asignar un valor por defecto
+    }
   }
 
   crearCuadrante() { console.log('Crear cuadrante'); }
